fad36b32659c861dbf1bf67059e41b57d05b8031
Josef Sebastian Duschl <josef-sebastian.Duschl@adesso.de>
2022-01-19 11:00:51 +0100

    Add the @Input() hero property

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
+
+
+// Add the @Input() hero property starts here
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+// Add the @Input() hero property ends here
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +12,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
+
+  // Add the @Input() hero property starts here
+   @Input() hero?: Hero;
+  // Add the @Input() hero property ends here
 
   constructor() { }
 
